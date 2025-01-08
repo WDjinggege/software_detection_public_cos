d31b20f5c06e7a35020aee801a716739d54f040b
FanK <1258846421@qq.com>
2025-01-08 10:33:25 +0800

    Default Changelist

--- frontend/src/views/login/Common.vue
+++ frontend/src/views/login/Common.vue
@@ -3,7 +3,7 @@
     <div class="content" style="margin: 0 auto">
       <div class="top">
         <div class="header">
-          <span class="title" style="color: white">耗材质量检测系统</span>
+          <span class="title" style="color: white">软件检测系统</span>
         </div>
         <div class="desc"></div>
       </div>
